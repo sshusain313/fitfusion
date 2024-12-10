a577bfd870fae396267e392d344b7cf0774374a0
shabahat <shabahatsyed101@gmail.com>
2024-12-10 09:24:15 +0530

    first commit

--- fit/src/utils/fetchData.js
+++ fit/src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		'X-RapidAPI-Key': 'ccc76f7075msh1d8385af9f680c3p1bf1abjsn01182e3d0d23',
 		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
 	}
 }
@@ -16,7 +16,7 @@ return data;
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'ccc76f7075msh1d8385af9f680c3p1bf1abjsn01182e3d0d23',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
